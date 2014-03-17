8ea70a1d7aa4f4df23e71906661fafcc46ea6094
J.D. DeVaughn-Brown <jddevaughn@cs.umass.edu>
2014-03-17 16:32:18 -0400

    Added test file for pygame

--- add.py
+++
@@ -1,6 +0,0 @@
-print("Enter a number: ")
-number1 = int(raw_input())
-print("Enter another number: ")
-number2 = int(raw_input())
-print("The sum is: " + str(number1+number2))
-print("Fresh new content")
